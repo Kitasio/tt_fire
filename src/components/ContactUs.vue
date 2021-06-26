6c2b4672db940227c2c072fa2071fdc6d3e07927
kitasio <kit.plotits@gmail.com>
2021-06-27 00:16:20 +0300

    kukold

--- src/components/ContactUs.vue
+++ src/components/ContactUs.vue
@@ -5,13 +5,13 @@
             <div>
                 <div class="uppercase text-5xl font-brand-bold lg:text-6xl mb-5 md:mb-7 2xl:text-7xl">contact us</div>
                 <div class="space-y-3 md:space-y-6">
-                    <input class="border-b focus:outline-none text-xl border-black w-full" type="text" placeholder="Name" required>
-                    <input class="border-b focus:outline-none text-xl border-black w-full" type="email" placeholder="Email" required>
-                    <input class="border-b focus:outline-none text-xl border-black w-full" type="text" placeholder="Telegram, Whatsapp, etc...">
-                    <textarea class="border-b transition-all duration-700 h-8 focus:h-32 focus:outline-none text-xl border-black w-full" id="txt" name="request" rows="1" placeholder="Describe your request..."></textarea>
+                    <input v-model="doc.name" class="border-b focus:outline-none text-xl border-black w-full" type="text" placeholder="Name" required>
+                    <input v-model="doc.email" class="border-b focus:outline-none text-xl border-black w-full" type="email" placeholder="Email" required>
+                    <input v-model="doc.messenger" class="border-b focus:outline-none text-xl border-black w-full" type="text" placeholder="Telegram, Whatsapp, etc...">
+                    <textarea v-model="doc.request" class="border-b transition-all duration-700 h-8 focus:h-32 focus:outline-none text-xl border-black w-full" id="txt" name="request" rows="1" placeholder="Describe your request..."></textarea>
                 </div>
             </div>
-            <div class="flex justify-end cursor-pointer">
+            <div @click="submit" class="flex justify-end cursor-pointer">
                 <div class="uppercase text-xl border-black border-2 px-7 py-1">submit</div>
             </div>
         </div>
@@ -21,12 +21,30 @@
 
 <script setup>
 import { ref, defineProps, defineEmit } from 'vue'
+import axios from 'axios'
+
 const props = defineProps(['isHidden'])
 const emits = defineEmit(['close'])
 
 const closeModal = () => {
     emits('close')
 }
+
+const submit = () => {
+    axios.post("http://178.154.205.40/tg/clientRequest", {
+        name: doc.value.name,
+        email: doc.value.email,
+        messenger: doc.value.messenger,
+        request: doc.value.request,
+    })
+}
+
+const doc = ref({
+    name: '',
+    email: '',
+    messenger: '',
+    request: '',
+})
 </script>
 
 <style>
